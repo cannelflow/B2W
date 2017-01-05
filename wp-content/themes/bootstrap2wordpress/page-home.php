bbdd851fe343b7f58948f58082245a85f0a8d773
cannelflow <mightyrocket@hotmail.com>
2017-01-05 17:37:04 +0000

    Lecture 56

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -32,7 +32,22 @@ $course_features_section_title                     = get_field('course_features_
 $course_features_section_body                      = get_field('course_features_section_body');
 
 $project_feature_section_title                     = get_field('project_feature_section_title');
-$project_feature_section_body                     = get_field('project_feature_section_body');
+$project_feature_section_body                      = get_field('project_feature_section_body');
+
+$watch_the_course_section_title                    = get_field('watch_the_course_section_title');
+$watch_the_course_section_url                      = get_field('watch_the_course_section_url');
+
+$instructor_section_title                          = get_field('instructor_section_title');
+$instructor_section_name                           = get_field('instructor_section_name');
+$instructor_section_bio                            = get_field('instructor_section_bio');
+$instructor_section_full_bio                       = get_field('instructor_section_full_bio');
+$twitter_username                                  = get_field('twitter_username');
+$facebook_username                                 = get_field('facebook_username');
+$google_plus                                       = get_field('google_plus');
+$student                                           = get_field('student');
+$review                                            = get_field('review');
+$course                                            = get_field('course');
+
 
 get_header(); ?>
 
@@ -271,8 +286,8 @@ get_header(); ?>
     <div class="container">
       <div class="row">
         <div class="col-sm-8 col-sm-offset-2">
-          <h2>Watch the Course Introduction</h2>
-          <iframe width="100%" height="415" src="//www.youtube.com/embed/q-mJJsnOHew" frameborder="0" allowfullscreen></iframe>
+          <h2><?php echo  $watch_the_course_section_title ; ?></h2>
+          <iframe width="100%" height="415" src="<?php echo $watch_the_course_section_url ; ?>" frameborder="0" allowfullscreen></iframe>
         </div>
         <!-- end col -->
       </div>
@@ -290,30 +305,22 @@ get_header(); ?>
         <div class="col-sm-8 col-md-6">
           <div class="row">
             <div class="col-lg-8">
-              <h2>Your Instructor <small>Brad Hussey</small></h2>
+              <h2><?php echo $instructor_section_title ;?> <small><?php echo $instructor_section_name ;?></small></h2>
             </div>
             <!-- end col -->
             <div class="col-lg-4">
-              <a href="https://twitter.com/bradhussey" class="badge social twitter" target="_blank"><i class="fa fa-twitter"></i></a>
-              <a href="https://facebook.com/bradhussey" class="badge social facebook" target="_blank"><i class="fa fa-facebook"></i></a>
-              <a href="https://plus.google.com/+BradHussey" class="badge social gplus" target="_blank"><i class="fa fa-google-plus"></i></a>
+              <a href="<?php echo $twitter_username  ;?>" class="badge social twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+              <a href="<?php echo $facebook_username  ;?>" class="badge social facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+              <a href="<?php echo $google_plus  ;?>" class="badge social gplus" target="_blank"><i class="fa fa-google-plus"></i></a>
             </div>
             <!-- end col -->
 
           </div>
           <!-- row -->
 
-          <p class="lead">A highly skilled professional, Brad Hussey is a passionate and experienced web designer, developer, blogger and digital entrepreneur.
-            <p>
+          <p class="lead"><?php echo $instructor_section_bio ;?><p>
 
-              <p>Hailing from North Of The Wall (Yellowknife, Canada), Brad made the trek to the Wet Coast (Vancouver, Canada) to educate and equip himself with the necessary skills to become a spearhead in his trade of solving problems on the web, crafting
-                design solutions, and speaking in code.</p>
-
-              <p>Brad's determination and love for what he does has landed him in some pretty interesting places with some neat people. He's had the privilege of working with, and providing solutions for, numerous businesses, big &amp; small, across the
-                Americas.</p>
-
-              <p>Brad builds custom websites, and provides design solutions for a wide-array of clientele at his company, Brightside Studios. He regularly blogs about passive income, living your life to the fullest, and provides premium quality web design
-                tutorials and courses for tens of thousands of amazing people desiring to master their craft.</p>
+              <?php echo $instructor_section_full_bio  ;?>
 
               <hr>
 
@@ -322,7 +329,7 @@ get_header(); ?>
                 <div class="col-xs-4">
                   <div class="num">
                     <div class="num-content">
-                      41,000+ <span>students</span>
+                      <?php echo $student  ;?>+ <span>students</span>
                     </div>
                     <!-- num-content -->
                   </div>
@@ -333,7 +340,7 @@ get_header(); ?>
                 <div class="col-xs-4">
                   <div class="num">
                     <div class="num-content">
-                      568 <span>reviews</span>
+                      <?php echo $review  ;?> <span>reviews</span>
                     </div>
                     <!-- num-content -->
                   </div>
@@ -344,7 +351,7 @@ get_header(); ?>
                 <div class="col-xs-4">
                   <div class="num">
                     <div class="num-content">
-                      8 <span>courses</span>
+                      <?php echo $course  ;?> <span>courses</span>
                     </div>
                     <!-- num-content -->
                   </div>
@@ -371,72 +378,36 @@ get_header(); ?>
 
         <div class="col-sm-8 col-sm-offset-2">
           <h2>What People Are Saying About Brad</h2>
+          
+          <?php 
+            query_posts(array( 
+                 'post_type' => 'testimonial',
+                 'showposts' => 10,
+              ) );  
+        ?>
+        
+        <?php while (have_posts()) : the_post(); ?>
 
-          <!-- TESTIMONIAL -->
+            <!-- TESTIMONIAL -->
           <div class="row testimonial">
             <div class="col-sm-4">
-              <img src="<?php bloginfo('stylesheet_directory') ; ?>/img/brennan.jpg" alt="Brennan">
+              <?php if(has_post_thumbnail()){
+                    the_post_thumbnail( 'medium' ); 
+                 }
+                 ?>
             </div>
             <!-- end col -->
             <div class="col-sm-8">
               <blockquote>
-                These videos are well created, concise, fast-paced, easy to follow, and just funny enough to keep you chuckling as you're slamming out lines of code. I've taken 3 courses from this instructor. Whenever I have questions he is right there with a simple
-                solution or a helpful suggestion to keep me going forward with the course work.
-                <cite>&mdash; Brennan, graduate of all of Brad's courses</cite>
+                <?php the_content() ; ?>
+                <cite>&mdash; <?php the_title() ; ?></cite>
               </blockquote>
             </div>
             <!-- end col -->
           </div>
           <!-- row -->
-
-          <!-- TESTIMONIAL -->
-          <div class="row testimonial">
-            <div class="col-sm-4">
-              <img src="<?php bloginfo('stylesheet_directory') ; ?>/img/ben.png" alt="Illustration of a man with a moustache">
-            </div>
-            <!-- end col -->
-            <div class="col-sm-8">
-              <blockquote>
-                I found Brad to be a great teacher, and a very inspiring person. It's clear he is very passionate about helping designers learn to code, and I look forward to more courses from him!
-                <cite>&mdash; Ben, graduate of Build a Website from Scratch with HTML &amp; CSS</cite>
-              </blockquote>
-            </div>
-            <!-- end col -->
-          </div>
-          <!-- row -->
-
-          <!-- TESTIMONIAL -->
-          <div class="row testimonial">
-            <div class="col-sm-4">
-              <img src="<?php bloginfo('stylesheet_directory') ; ?>/img/aj.png" alt="Illustration of a man with a beard">
-            </div>
-            <!-- end col -->
-            <div class="col-sm-8">
-              <blockquote>
-                Brad is amazing and I honestly think he's the best tutor of all the courses I have taken on Udemy. Will definitely be following him in the future. Thanks Brad!
-                <cite>&mdash; AJ, graduate of Code a Responsive Website with Bootstrap 3</cite>
-              </blockquote>
-            </div>
-            <!-- end col -->
-          </div>
-          <!-- row -->
-
-          <!-- TESTIMONIAL -->
-          <div class="row testimonial">
-            <div class="col-sm-4">
-              <img src="<?php bloginfo('stylesheet_directory') ; ?>/img/ernest.png" alt="Illustration of a man with a goatee">
-            </div>
-            <!-- end col -->
-            <div class="col-sm-8">
-              <blockquote>
-                Brad is an excellent instructor. His content is super high quality, and you can see the love and care put into every section. The tutorials are the perfect length, and you feel like your doing something right out the gate! I really can't believe this
-                is free. I highly recommend taking advantage of this course.
-                <cite>&mdash; Ernest, graduate of Code Dynamic Websites with PHP</cite>
-              </blockquote>
-            </div>
-            <!-- end col -->
-          </div>
-          <!-- row -->
+            
+        <?php endwhile;?>
 
         </div>
         <!-- end col -->
