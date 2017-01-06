361981071d0450c6a43335dba0504f16c07d2e57
cannelflow <mightyrocket@hotmail.com>
2017-01-06 14:59:45 +0000

    Lecture 62

--- wp-content/themes/bootstrap2wordpress/index.php
+++ wp-content/themes/bootstrap2wordpress/index.php
@@ -11,46 +11,54 @@
  *
  * @package Bootstrap_To_Wordpress
  */
-
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+        <h1 class="page-title">Blog</h1>
+</section>
 
-		<?php
-		if ( have_posts() ) :
+  <!-- Blog Content -->
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
+    <div class="container">
+        <div class="row" id="primary">
+            <main id="content" class="col-sm-8" role="main">
+            	
+            	<?php
+	           	if ( have_posts() ) :
+			       if ( is_home() && ! is_front_page() ) : ?>
+				   <header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+				   </header>
 
-			<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			       <?php
+			       endif;
+			        /* Start the Loop */
+			        while ( have_posts() ) : the_post();
+			      	/*
+				    * Include the Post-Format-specific template for the content.
+				    * If you want to override this in a child theme, then include a file
+				    * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				    */
+				    get_template_part( 'template-parts/content', get_post_format() );
+			        endwhile;
+			        the_posts_navigation();
+		            else :
+			        get_template_part( 'template-parts/content', 'none' );
+		            endif; ?>
+            	
+            </main><!-- #main -->
+            <!-- SIDEBAR -->
+            <aside class="col-sm-4">
+            	
+            	<?php get_sidebar() ; ?>
+            	
+            </aside>
+            <!-- SIDEBAR -->
+        </div><!-- #primary -->
+    </div><!-- .container -->    
+            	
+  <!-- Blog Content -->          	
 
 <?php
-get_sidebar();
+
 get_footer();
